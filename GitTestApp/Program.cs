085c748b0cc4426a07cc3f9f7ed0cbfb421fc96c
hushi1991 <davidbechhansen@gmail.com>
2019-02-15 10:18:26 +0100

    Adding test message
    Test message added.

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -7,6 +7,7 @@ namespace GitTestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I am testing git");
         }
     }
 }
